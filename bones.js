fde6c04e85ea74cb4ae835008a90a39a78436036
Adam Wolff <adam@electra>
2022-01-20 11:53:50 -0800

    Passing strings to Rust

--- bones.js
+++ bones.js
@@ -10,13 +10,18 @@ const importObject = {
 WebAssembly.instantiateStreaming(fetch("bones.wasm"), importObject).then(m => {
     wasmModule = m;
     const add = wasmModule.instance.exports.add;
+
     console.log("add(2,3)", add(2, 3));
 });
 
+function write(key, val) {
+    wasmModule.instance.exports.write(toCString(key), val);
+}
+
 function toCString(s) {
     const stringSize = s.length + 1;
-    const p = instanceExports.alloc_cstring(stringSize);
-    const m = new Uint8Array(wasmModule.memory.buffer, p, stringSize);
+    const p = wasmModule.instance.exports.alloc_cstring(stringSize);
+    const m = new Uint8Array(wasmModule.instance.exports.memory.buffer, p, stringSize);
     for (let i = 0; i < s.length; i++)
         m[i] = s.charCodeAt(i);
     m[s.length] = 0;
@@ -27,7 +32,7 @@ function fromCString(ptr) {
     const m = new Uint8Array(wasmModule.instance.exports.memory.buffer, ptr);
     let s = "";
     while (m[s.length] != 0) {
-        s += String.fromCharCode(m[i]);
+        s += String.fromCharCode(m[s.length]);
     }
     wasmModule.instance.exports.dealloc_cstring(ptr);
     return s;
--- bones.rs
+++ bones.rs
@@ -27,6 +27,16 @@ pub extern fn add(x: i32, y: i32) -> i32 {
     log(format!("add {} {}", x, y));
     x + y
 }
+
+#[no_mangle]
+pub extern fn write(c_key: *mut c_char, val: i32) {
+    let key = unsafe {
+        CString::from_raw(c_key).into_string()
+    };
+
+    log(format!("write {} {}", key.unwrap(), val));
+}
+
 fn log(s:String) {
     let c_string = CString::new(s).unwrap();
     let p: *mut c_char = c_string.into_raw();
