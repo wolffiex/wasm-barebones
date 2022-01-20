6e8e89d97117e1d0a18c71cfecc7f28be4a9fd10
Adam Wolff <adam@electra>
2022-01-20 11:43:33 -0800

    CLeanup

--- bones.js
+++ bones.js
@@ -1,7 +1,7 @@
 var wasmModule = null;
 const importObject = {
     env: {
-        consoleLog: function(p) {
+        consoleLog: function (p) {
             console.log("wasm says:", fromCString(p));
         }
     }
@@ -9,16 +9,25 @@ const importObject = {
 
 WebAssembly.instantiateStreaming(fetch("bones.wasm"), importObject).then(m => {
     wasmModule = m;
-    var add = wasmModule.instance.exports.add;
-    console.log("add(2,3)", add(2,3));
+    const add = wasmModule.instance.exports.add;
+    console.log("add(2,3)", add(2, 3));
 });
 
+function toCString(s) {
+    const stringSize = s.length + 1;
+    const p = instanceExports.alloc_cstring(stringSize);
+    const m = new Uint8Array(wasmModule.memory.buffer, p, stringSize);
+    for (let i = 0; i < s.length; i++)
+        m[i] = s.charCodeAt(i);
+    m[s.length] = 0;
+    return p;
+}
+
 function fromCString(ptr) {
-    var m = new Uint8Array(wasmModule.instance.exports.memory.buffer, ptr);
-    var s = "";
-    let i = 0;
-    while (m[i] != 0) {
-        s += String.fromCharCode(m[i++]);
+    const m = new Uint8Array(wasmModule.instance.exports.memory.buffer, ptr);
+    let s = "";
+    while (m[s.length] != 0) {
+        s += String.fromCharCode(m[i]);
     }
     wasmModule.instance.exports.dealloc_cstring(ptr);
     return s;
--- bones.rs
+++ bones.rs
@@ -8,21 +8,8 @@ extern "C" {
 }
 
 #[no_mangle]
-pub extern fn add(x: i32, y: i32) -> i32 {
-    log(format!("add {} {}", x, y));
-    x + y
-}
-fn log(s:String) {
-    let c_string = CString::new(s).unwrap();
-    let p: *mut c_char = c_string.into_raw();
-    unsafe {
-        consoleLog(p);
-    }
-}
-
-#[no_mangle]
-pub fn alloc_cstring(string_size: usize) -> *mut c_char {
-    let mut buf = vec![0 as u8; string_size+1];
+pub fn alloc_cstring(size: i32) -> *mut c_char {
+    let mut buf = vec![0 as u8; size as usize + 1];
     let p = buf.as_mut_ptr();
     mem::forget(buf);
     p as *mut c_char
@@ -33,4 +20,17 @@ fn dealloc_cstring(p: *mut c_char) {
     let _ = unsafe {
         CString::from_raw(p)
     };
+}
+
+#[no_mangle]
+pub extern fn add(x: i32, y: i32) -> i32 {
+    log(format!("add {} {}", x, y));
+    x + y
+}
+fn log(s:String) {
+    let c_string = CString::new(s).unwrap();
+    let p: *mut c_char = c_string.into_raw();
+    unsafe {
+        consoleLog(p);
+    }
 }
