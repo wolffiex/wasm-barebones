defc5c1280d70dfc08397949fce5e2fc970bfb2f
Adam Wolff <adam@electra>
2022-01-20 11:18:11 -0800

    Logging

--- bones.js
+++ bones.js
@@ -1,4 +1,25 @@
-WebAssembly.instantiateStreaming(fetch("bones.wasm")).then(wasmModule => {
+var wasmModule = null;
+const importObject = {
+    env: {
+        consoleLog: function(p) {
+            console.log("wasm says:", fromCString(p));
+        }
+    }
+}
+
+WebAssembly.instantiateStreaming(fetch("bones.wasm"), importObject).then(m => {
+    wasmModule = m;
     var add = wasmModule.instance.exports.add;
     console.log("add(2,3)", add(2,3));
 });
+
+function fromCString(ptr) {
+    var m = new Uint8Array(wasmModule.instance.exports.memory.buffer, ptr);
+    var s = "";
+    let i = 0;
+    while (m[i] != 0) {
+        s += String.fromCharCode(m[i++]);
+    }
+    wasmModule.instance.exports.dealloc_cstring(ptr);
+    return s;
+}
--- bones.rs
+++ bones.rs
@@ -3,14 +3,26 @@ use std::mem;
 // use std::ptr;
 use std::os::raw::c_char;
 
-#[no_mangle]
-pub extern fn add(x: i32, y: i32) -> i32 {
-    x + y
+extern "C" {
+    fn consoleLog(p: *mut c_char);
 }
 
 #[no_mangle]
-pub fn alloc_cstring(size: usize) -> *mut c_char {
-    let mut buf = vec![0 as u8; size];
+pub extern fn add(x: i32, y: i32) -> i32 {
+    log(format!("add {} {}", x, y));
+    x + y
+}
+fn log(s:String) {
+    let c_string = CString::new(s).unwrap();
+    let p: *mut c_char = c_string.into_raw();
+    unsafe {
+        consoleLog(p);
+    }
+}
+
+#[no_mangle]
+pub fn alloc_cstring(string_size: usize) -> *mut c_char {
+    let mut buf = vec![0 as u8; string_size+1];
     let p = buf.as_mut_ptr();
     mem::forget(buf);
     p as *mut c_char
